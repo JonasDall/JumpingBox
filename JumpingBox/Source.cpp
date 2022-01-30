228fc1b70ff78e01997ff3665620cfe093d682fc
Jonas Dall <jonaskd@live.no>
2022-01-30 17:08:15 +0100

    Basic setup

--- JumpingBox/Source.cpp
+++ JumpingBox/Source.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <chrono>
+#include <vector>
+
+sf::Color Grey(0x00CBDCCF);
+sf::Color FaintBlue(0x00BDC9FF);
+sf::Color FaintRed(0x00DE8C8A);
 
 class Wall
 {
@@ -10,39 +16,77 @@ public:
 	{
 		shape.setSize(size);
 		shape.setPosition(location);
-		shape.setFillColor(sf::Color(0x00BDC9FF));
+		shape.setFillColor(FaintBlue);
+	}
+
+	void display(sf::RenderWindow& window)
+	{
+		window.draw(shape);
 	}
 };
 
 class Player
 {
 	sf::RectangleShape rect;
+	double movementSpeed{};
+	sf::Vector2f force{};
 
 public:
-	Player(sf::Vector2f size)
+	Player(sf::Vector2f size, double speed) : movementSpeed{speed}
 	{
 		rect.setSize(size);
-		rect.setFillColor(sf::Color::Red);
+		rect.setFillColor(FaintRed);
 	}
 
-	void addForce(sf::Vector2f direction)
+	void addInput(sf::Event& event)
 	{
+		switch (event.key.code)
+		{
+		case sf::Keyboard::D :
+			addForce(sf::Vector2f(movementSpeed, 0.f));
+			break;
 
+		case sf::Keyboard::A :
+			addForce(sf::Vector2f(movementSpeed * -1, 0.f));
+			break;
+
+		case sf::Keyboard::Space :
+			addForce(sf::Vector2f(0.f, movementSpeed));
+			break;
+		}
+	}
+
+	void addForce(sf::Vector2f newForce)
+	{
+		force.x += newForce.x;
+		force.y += newForce.y;
 	}
 
 	void updatePosition()
 	{
 		
 	}
+
+	void display(sf::RenderWindow& window)
+	{
+		window.draw(rect);
+	}
 };
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "Box");
+	std::vector<Wall*> walls;
+
+	Player player(sf::Vector2f(20.f, 20.f), 100.f);
+
+	walls.push_back(new Wall(sf::Vector2f(20.f, 20.f), sf::Vector2f(250.f, 250.f)));
 
 	while (window.isOpen())
 	{
 		sf::Event event;
+
+		//Input loop
 
 		while (window.pollEvent(event))
 		{
@@ -50,15 +94,35 @@ int main()
 			{
 				window.close();
 			}
+
+			if (event.type == sf::Event::KeyPressed)
+			{
+
+			}
 		}
 
-		window.clear();
+		//Logic loop
+
+
+
+		// Render loop
+		window.clear(Grey);
+
+		for (unsigned int i {}; i < walls.size(); ++i)
+		{
+			walls[i]->display(window);
+		}
+		
+		player.display(window);
 
 		window.display();
-
 	}
 
-
+	for (unsigned int i{}; i < walls.size(); ++i)
+	{
+		delete walls[i];
+		walls[i] = 0;
+	}
 
 	return 0;
 }
